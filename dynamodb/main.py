85b1a49913d6a4a4c3afb0f62bc868f79c8a5b19
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2025-05-15 22:37:17 -0500

    Crud operation in dynamodb

---
+++ dynamodb/main.py
@@ -0,0 +1,62 @@
+import boto3
+
+
+dynamodb = boto3.client("dynamodb", region_name="us-east-1")
+
+# creaing a new table
+# table = dynamodb.create_table(
+#     TableName='Users',
+#     KeySchema=[
+#         {'AttributeName': 'UserID', 'KeyType': 'HASH'},   # Partition key
+#         {'AttributeName': 'Timestamp', 'KeyType': 'RANGE'} # Sort key
+#     ],
+#     AttributeDefinitions=[
+#         {'AttributeName': 'UserID', 'AttributeType': 'S'},
+#         {'AttributeName': 'Timestamp', 'AttributeType': 'N'}
+#     ],
+#     ProvisionedThroughput={
+#         'ReadCapacityUnits': 5,
+#         'WriteCapacityUnits': 5
+#     }
+# )
+
+
+# describing the table
+# response = dynamodb.describe_table(TableName='Users')
+
+
+# PUT item in table
+try:
+    response = dynamodb.put_item(
+        TableName="Users",
+        Item={
+            "UserID": {"S": "12345"},
+            "Timestamp": {"N": "1234567890"},
+            "Name": {"S": "John Doe"},
+            "Age": {"N": "30"},
+        },
+        ConditionExpression="attribute_not_exists(UserID) AND attribute_not_exists(Timestamp)",
+    )
+    print("Item added:", response)
+except dynamodb.exceptions.ClientError as e:
+    print("Item already exists")
+
+
+# getting item from table
+response = dynamodb.get_item(
+    TableName="Users", Key={"UserID": {"S": "12345"}, "Timestamp": {"N": "1234567890"}}
+)
+print(response.get("Item", "No item found"))
+
+# update the item
+response = dynamodb.update_item(
+    TableName="Users",
+    Key={"UserID": {"S": "12345"}, "Timestamp": {"N": "1234567890"}},
+    UpdateExpression="SET Age = :val1",
+    ExpressionAttributeValues={":val1": {"N": "31"}},
+)
+
+# delete the item
+response = dynamodb.delete_item(
+    TableName="Users", Key={"UserID": {"S": "12345"}, "Timestamp": {"N": "1234567890"}}
+)
